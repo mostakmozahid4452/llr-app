ec880bc1b2e55c73d8d176bd0ee3ad8e1c43f3c4
mostakmozahid4452 <mostaksagor4452@gmail.com>
2026-08-16 12:50:40 +0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -6,7 +6,9 @@ const SHELL_FILES = [
   './index.html',
   './manifest.json',
   './icon-192.png',
-  './icon-512.png'
+  './icon-512.png',
+  './icon-192-maskable.png',
+  './icon-512-maskable.png'
 ];
 
 self.addEventListener('install', (event) => {
